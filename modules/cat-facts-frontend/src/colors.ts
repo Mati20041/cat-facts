2a640cda5e1381debb2d7d539451f39109a46238
Mati20041 <mat20041+github@gmail.com>
2022-06-29 18:40:58 +0200

    fixed colors

--- modules/cat-facts-frontend/src/colors.ts
+++ modules/cat-facts-frontend/src/colors.ts
@@ -1,6 +1,6 @@
 export const Colors = {
-  navLine: '#61dafb',
-  navBackground: 'F0FFFFFF',
-  error: 'FF0000FF',
-  shadow: '5F9EA0FF',
+  navLine: '#61DAFB',
+  navBackground: '#F0FFFFFF',
+  error: '#FF0000FF',
+  shadow: '#5F9EA0FF',
 };
