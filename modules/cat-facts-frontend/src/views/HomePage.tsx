ac66fcb22813a68c8523ca56f231c79f48caa7c2
Mateusz <mat20041@gmail.com>
2022-08-26 19:06:22 +0200

    Added a comment to HomePage

--- modules/cat-facts-frontend/src/views/HomePage.tsx
+++ modules/cat-facts-frontend/src/views/HomePage.tsx
@@ -5,6 +5,7 @@ import { CatTiles } from '../components/homePage/CatTiles';
 
 export const HomePage = () => {
   const { user } = useAuthentication();
+  // This should be a separate view/route with an authentication guard (e. <AuthenticatedRoute .../>)instead of having this logic over here
   if (user?.username == null) {
     return <NotLoggedUser />;
   }
